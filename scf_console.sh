0f59e4ec4c545a1e0e063141610f190e70e631f5
zhangxuezheng <zhang.xuezheng@foxmail.com>
2023-01-21 10:58:00 +0800

    build: run migrate automatically

--- scf_console.sh
+++ scf_console.sh
@@ -1,9 +1,9 @@
 yum install -y gcc python3-devel mysql-devel
 pip3 install -r requirements.txt -t .
-#pip3 install mysql-connector-python -t .
 pip3 install contextvars -t .
 pip3 install django-environ -t .
 pip3 install django-cors-headers -t .
 pip3 install mysqlclient -t .
 pip3 install gunicorn -t .
+python3 manage.py migrate
 echo '务必注意不要部署函数！'
